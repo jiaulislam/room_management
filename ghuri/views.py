aa1fbdc4917e68a487116ead928508b28bf78525
w4nn4b3cod3r <kmjiaulislam@gmail.com>
2021-01-16 23:08:21 +0600

    dashboard view created

--- ghuri/views.py
+++ ghuri/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def dashboard(request):
+    view_context = {
+        'title': 'Dashboard',
+    }
+    return render(request, 'ghuri/dashboard.html', view_context)
