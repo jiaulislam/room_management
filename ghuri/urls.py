0238583cb77cfbbc2bdd4266806667c15e585e86
w4nn4b3cod3r <kmjiaulislam@gmail.com>
2021-01-17 14:14:59 +0600

    all views template basic finished and urls connected

--- ghuri/urls.py
+++ ghuri/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard')
+    path('', views.dashboard, name='dashboard'),
+    path('add-expense/', views.add_expense, name='add_expense'),
+    path('add-meal/', views.add_meal, name='add_meal'),
+    path('list-expenses/', views.list_expenses, name='list_expenses'),
+    path('list-meals/', views.list_meals, name='list_meals'),
 ]
--- ghuri/views.py
+++ ghuri/views.py
@@ -6,3 +6,31 @@ def dashboard(request):
         'title': 'Dashboard',
     }
     return render(request, 'ghuri/dashboard.html', view_context)
+
+
+def add_expense(request):
+    view_context = {
+        'title': 'Add Expense',
+    }
+    return render(request, 'ghuri/add_expense.html', view_context)
+
+
+def add_meal(request):
+    view_context = {
+        'title': 'Add Meals',
+    }
+    return render(request, 'ghuri/add_meal.html', view_context)
+
+
+def list_expenses(request):
+    view_context = {
+        'title': 'Expenses List',
+    }
+    return render(request, 'ghuri/list_expenses.html', view_context)
+
+
+def list_meals(request):
+    view_context = {
+        'title': 'Meals List',
+    }
+    return render(request, 'ghuri/list_meals.html', view_context)
