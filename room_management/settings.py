8e7f1b98f742762f1a19477a286aed2a9923c9c2
w4nn4b3cod3r <kmjiaulislam@gmail.com>
2021-01-22 01:26:21 +0600

    redirect to dashboard after login

--- room_management/settings.py
+++ room_management/settings.py
@@ -121,5 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = 'index-ghuri'
+LOGIN_REDIRECT_URL = 'dashboard-ghuri'
 LOGIN_URL = 'login'
